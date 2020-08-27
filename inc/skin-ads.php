736d405a6df6c5e4610bc6c070706d3cca010e09
Iman Linuxs <student.linuxs@gmail.com>
2020-08-27 16:31:36 +0700

    Add files via upload

---
+++ inc/skin-ads.php
@@ -0,0 +1,14 @@
+
+<div class="skin-ads">
+  <div>
+    <div class="clearenter">
+      <div class="skin-ads-left clearenter">
+        <iframe title="iklan" scrolling="no" frameborder="0" marginheight="0" marginwidth="0" src="iklan/inc/skin.php" width="160" height="600"></iframe>
+      </div>
+    
+      <div class="skin-ads-right clearenter">
+        <iframe title="iklan" scrolling="no" frameborder="0" marginheight="0" marginwidth="0" src="iklan/inc/skin.php" width="160" height="600"></iframe>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="id">
+<base href="/skin-ads/" />
+<head>
+<title>SKIN-ADS</title>
+<meta charset="utf-8">
+<meta name="robots" content="noindex, follow">        
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="mobile-web-app-capable" content="yes">
+<meta name="HandheldFriendly" content="true" />
+<meta name="apple-touch-fullscreen" content="yes" />
+<link href="img/favicon.ico" rel="icon" type="image/ico" />
+</head>
+<body>
+
+<link rel="stylesheet" type="text/css" href="css/base.css" media="screen">
+<link rel="stylesheet" type="text/css" href="css/base-mobile.css" media="(max-width:1023px)">
+<link rel="stylesheet" type="text/css" href="css/base-desktop.css" media="(min-width:1024px)">
+
+
+<header>
+  
+</header>
+
+<?php require ('inc/skin-ads.php')?>
+
+    <div class="site-container content_center">
+
+        <span>
+          <div> 
+            <div class="content_center">
+              <h1>Container Website Lo!</h1>
+            </div>
+          </div>
+        </span>
+
+    </div>
+
+
+</body>
+</html>
